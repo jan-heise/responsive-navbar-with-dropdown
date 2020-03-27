5e75de0a3c946e4c14d1cd57f77551828a489bd4
JKH <jan-kevin.heise@travelbasys.de>
2020-03-27 15:07:20 +0100

    Removed unused configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        'black-transparent': {
-          '20': 'rgba(0,0,0,0.2)',
-          '40': 'rgba(0,0,0,0.4)',
-          '60': 'rgba(0,0,0,0.6)',
-          '80': 'rgba(0,0,0,0.8)',
-        },
-        fontFamily: {
-          sans: ['Inter var'],
-        },
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [
